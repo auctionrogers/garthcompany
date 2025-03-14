a0b127c85430198ba164fa238af47964e3cdda27
Justin Rogers <info@garthbid.com>
2025-03-14 15:56:13 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,25 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCN-hqm2-S6LD6UxdtSVdkY0p9ZL5bNM94",
-    authDomain: "garth-company.firebaseapp.com",
-    projectId: "garth-company",
-    storageBucket: "garth-company.appspot.com",
-    messagingSenderId: "881756283121",
-    appId: "1:881756283121:web:e1d08a1e59ce5724070950",
-    measurementId: "G-67ZZ85XE3Q"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Initialize Firestore
-  const db = firebase.firestore();
+  apiKey: "AIzaSyCN-hqm2-S6LD6UxdtSVdkY0p9ZL5bNM94",
+  authDomain: "garth-company.firebaseapp.com",
+  projectId: "garth-company",
+  storageBucket: "garth-company.appspot.com",
+  messagingSenderId: "881756283121",
+  appId: "1:881756283121:web:e1d08a1e59ce5724070950"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Add a test document directly from this file to verify Firebase connection
+db.collection("test").add({
+  message: "Connection test",
+  timestamp: new Date()
+})
+.then((docRef) => {
+  console.log("Test document written with ID: ", docRef.id);
+})
+.catch((error) => {
+  console.error("Error adding test document: ", error);
+});
